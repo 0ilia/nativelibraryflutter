3ecb87dc15ad9c2ef305ec4d945538a3f355d9c0
Ilya Martinkevich <ilia1mail2martinkevi4@gmail.com>
2020-04-20 00:51:09 +0300

    fix bug send to server

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,25 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
+// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      home: MyHomePage(title: 'Ilya Flutter'),
+      home: MyHomePage(title: 'Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
+
 
   final String title;
 
@@ -27,12 +30,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _startMsg = "";
   String _string ="";
-  String _endMsg = "";
-  String _req="";
+  String _startMsg ="";
+  String _req;
   String _res = "";
-  String _cryptedString ="";
+  String _cryptedString = "";
   final textControl = TextEditingController();
 
   static const platform = const MethodChannel('channel.ilya.crypto');
@@ -47,8 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // ignore: missing_return
-  Future<void> getResponse() {
+
+Future<void> getResponse() {
     Duration(milliseconds: 300);
     String _algorithm =
         "belt-hash"; // Выбор алгоритма
@@ -57,51 +59,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   }
 
-  Future<void> getHashString() async {
+
+  //Send request to server
+ /*
+  Future<void> getResponse() async {
     _string = textControl.text;
-    _req = "https://10.0.2.2:3000";
+    _req = "http://10.0.2.2:3001/"+_string;
+    print(_startMsg);
     var response = await http.get(_req);
     setState(() {
-      _res = response.body;
+      _cryptedString = response.body;
     });
-
-  }
-
+  }*/
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              _req,
-              style: Theme.of(context).textTheme.display1,
-            ),
-            RaisedButton(
-              onPressed: getHashString,
-              child: const Text(
-                  'Отправиь запрос на сервер',
-                  style: TextStyle(fontSize: 20)
-              ),
-            ),
             TextField(
               controller: textControl,
               decoration: InputDecoration(labelText: 'Enter a string'),
-            ),
-            RaisedButton(
-                onPressed: getResponse,
-              child: const Text(
-                  'Зашифровать нативной библиотекой',
-                  style: TextStyle(fontSize: 20)
-              ),
             ),
             Text(
               _cryptedString,
@@ -110,7 +93,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: getResponse,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
